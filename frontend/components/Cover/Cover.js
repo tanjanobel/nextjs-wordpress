4955897d0c6127531cddc96c3bfc6442900e0899
Tanja Nobel <tanja@nobel.swiss>
2022-11-30 11:32:34 +0100

    Cover component, style with Tailwind CSS & render images with NextJS Image

---
+++ frontend/components/Cover/Cover.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export const Cover = ({ children, background }) => {
+    return (
+        <div className="h-screen bg-slate-800 relative min-h-[400px] flex justify-center items-center">
+            <Image
+                alt="Cover"
+                src={background}
+                layout="fill"
+                objectFit="cover"
+                className="mix-blend-soft-light"
+            />
+            {children}
+        </div>
+    );
+};
