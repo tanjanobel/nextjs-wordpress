d9d1c9c427db0a872e22e283d7938d794fcc3817
Tanja Nobel <tanja@nobel.swiss>
2022-11-30 10:15:49 +0100

    Set up first query for WordPress data with GraphQL

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,26 @@
-export default function Home() {
+import { gql } from "@apollo/client";
+import client from "../client";
+
+export default function Home(props) {
+    console.log("PROPS: ", props);
     return <div>Next JS &amp; WordPress course.</div>;
 }
+
+export const getStaticProps = async () => {
+    const { data } = await client.query({
+        query: gql`
+            query NewQuery {
+                pages {
+                    nodes {
+                        title
+                    }
+                }
+            }
+        `,
+    });
+    return {
+        props: {
+            myexampleprop: "test",
+        },
+    };
+};
